a48fa6a505d7e0af35f621028b0e3f02f2a691f0
Teja Pagala <>
2020-03-18 00:17:32 -0400

    new line

--- 02-print.sh
+++ 02-print.sh
@@ -17,3 +17,20 @@ echo -e "Hello\nTeja"
 # echo with tab space 
 
 echo -e "Hello\t\tWorld"
+
+## To print output with colors, we have to use this color code.
+
+## color         ForeGround       BackGround
+
+# Red             31                   41
+# Green           32                   42
+# yellow          33                   43
+# Blue            34                   44
+# magenta         35                   45
+# cyan            36                   46
+
+# syntax for printing color
+
+# echo -e "\e[colorcodem message"
+
+echo -e "\e[35m Hello Teja in magenta color"
