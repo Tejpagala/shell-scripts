76e3a619ea45b30db50ee15464acf9164f89147a
Teja Pagala <>
2020-03-18 00:43:34 -0400

    new line

---
+++ 03-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# what is a variable 
+
+# When you assign a name to any data, Then it is called a variable.
+
+# syntax
+# VAR = DATA
+
+NAME = TEJA
+AGE = 25
+
+echo "Hello, My name is ${NAME}"
+echo "and my age is ${AGE}"
+ 
