7c8318a75974d36f011aeabc25909722bd011877
Teja Pagala <>
2020-03-23 16:32:29 -0400

    while loop

---
+++ 10-while-loop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# while [ expression ]; do commands ; done
+# while command; do command; done
+
+sleep 50 &
+
+while `ps -ef | grep -sleep | grep -v grep &>/dev/null`
+do 
+   echo "sleep command is running"
+   sleep 1
+done
